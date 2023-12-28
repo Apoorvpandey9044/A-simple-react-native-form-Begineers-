6d057b8d10abb8f3343b4230ee422e5aea69d5e3
Apoorv Pandey <92255734+Apoorvpandey9044@users.noreply.github.com>
2023-12-29 00:52:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -18,3 +18,52 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// TouchableHighlight button code 
+import React from  'react';
+import {Text , View , TouchableHighlight, StyleSheet} from 'react-native'
+
+const App =()=>{
+  return (
+    <View style = {styles.main}>
+      <TouchableHighlight onPress={() => alert('You pressed the button!')}>
+        <Text style={[styles.button, styles.primary]}>primary</Text>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => alert('You pressed the button!')}>
+        <Text style={[styles.button, styles.success]}>Success</Text>
+      </TouchableHighlight>
+      
+      <TouchableHighlight onPress={() => alert('You pressed the button!')}>
+        <Text style={styles.button}>Button</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex:1
+},
+button:{
+  backgroundColor: '#bbb',
+  color: "#fff",
+  fontSize:24,
+  textAlign:'center',
+  padding:10,
+  margin:10,
+  borderRadius:10,
+  shadowColor:'black',
+  elevation:10,
+  shadowOpacity:1
+},
+success:{backgroundColor:'red'},
+primary:{backgroundColor:'blue'}
+})
+
+export default App;
+
+
+//TouchableOpacity button code 
+
+
