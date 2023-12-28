c07c3a5d8bbbbb7268b6deb14e6fd8c55d1ac52b
Apoorv Pandey <92255734+Apoorvpandey9044@users.noreply.github.com>
2023-12-29 03:10:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -196,3 +196,37 @@ radioWrapper:{flexDirection:'row', alignItems:'centre'}
 
 export default App;
 
+
+// Loader 
+
+import React from  'react';
+import {Text , View , ActivityIndicator, StyleSheet, Button} from 'react-native'
+
+const App =()=>{
+
+  const [show, setShow]=useState(false);
+  const displayLoader=()=>{
+    setShow(true);
+
+  setTimeout(() =>{
+setShow(false)
+  }, 5000);
+  }
+return(
+  <View style={styles.main}>
+    <ActivityIndicator size="small" color="#000"/>
+    <Button title='show loader' onPress={displayLoader}></Button>
+  </View>
+)
+}
+
+const styles = StyleSheet.create({
+  main:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+}
+})
+
+export default App;
+
