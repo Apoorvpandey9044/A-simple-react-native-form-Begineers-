a9d41eb0182a77c1cd00415e284aba11a13bd416
Apoorv Pandey <92255734+Apoorvpandey9044@users.noreply.github.com>
2023-12-29 01:12:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -65,5 +65,65 @@ export default App;
 
 
 //TouchableOpacity button code 
+import React from  'react';
+import {Text , View , TouchableOpacity, StyleSheet} from 'react-native'
 
+const App =()=>{
+
+  const [selectedRadio, setselectedRadio] = useState(1)
+  return (
+    <View style = {styles.main}>
+      <TouchableOpacity onPress={()=>setselectedRadio(1)}>
+        <View style = {styles.radioWrapper}>
+          <View style={styles.radio}>
+            {
+              selectedRadio===1 ? <View style={styles.radioBg}></View>:null
+            }
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=>setselectedRadio(2)}>
+        <View style = {styles.radioWrapper}>
+          <View style={styles.radio}>
+          {
+              selectedRadio===2 ? <View style={styles.radioBg}></View>:null
+            }
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent:'center'
+},
+radioText:{
+  fontSize:20,
+},
+radio:{
+  height:40,
+  width:40,
+  borderColor:'black',
+  borderWidth:3,
+  borderRadius:20,
+  margin:10
+},
+radioBg:{
+  backgroundColor:'balck',
+  height:25,
+  width:25,
+  borderRadius:(20),
+  margin:4
+},
+radioWrapper:{flexDirection:'row', alignItems:'centre'}
+})
+
+export default App;
 
