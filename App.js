bef917903787f647b1e765d7977e3c358fdf0109
Apoorv Pandey <92255734+Apoorvpandey9044@users.noreply.github.com>
2024-01-03 00:43:42 +0530

    Update App.js

--- App.js
+++ App.js
@@ -914,3 +914,37 @@ const styles=StyleSheet.create({
 })
 
 export default App;
+
+// Search icon with api integration 
+
+import React, { useState } from 'react';
+import {Text, View, Button, TextInput} from 'react-native';
+
+const App =()=>{
+  const [data,setData] = useState([]);
+  const searchUser = async (text)=>{
+   const url=`http://192.168.1.3:3000/users?q=${text}`;
+   let result = await fetch(url);
+   result = await result.json();
+   if(result){
+     setData(result)
+   }
+
+  }
+  return(
+    <View style={{flex:1}}>
+      <TextInput style={{borderColor:"skyblue", borderWidth:1, margin:15, fontSize:25, top:22}} placeholder={"Search"} onChangeText={(text)=>searchUser(text)}></TextInput>
+      {
+        data.length ? 
+        data.map((item)=><View style={{padding:10, flexDirection:'row', justifyContent:'space-between'}}>
+          <Text>{item.name}</Text>
+          <Text>{item.age}</Text>
+          <Text>{item.email}</Text>
+        </View>):null
+      }
+    </View>
+  )
+};
+
+
+export default App;
